0d50fd13f8578037ece185a9f12b261ecb6c08ed
Kamil <k.medrala99@gmail.com>
2023-05-05 11:07:12 +0200

    [ECHO-6] video cover

--- flexible-templates/columns_industries.php
+++ flexible-templates/columns_industries.php
@@ -4,6 +4,7 @@
 	$description        = wp_kses_post( get_sub_field( 'description' ) );
 	$industries = get_sub_field( 'industries' );
 	$video_iframe = get_sub_field( 'video_iframe' );
+	$video_cover_img = get_sub_field( 'video_cover_img' );
 	$video_title  = wp_kses_post( get_sub_field( 'video_title' ) );
 	$header2        = wp_kses_post( get_sub_field( 'header2' ) );
 	$content        = wp_kses_post( get_sub_field( 'content' ) );
@@ -37,6 +38,27 @@
 				<?php if ( $video_iframe ) : ?>
 					<div class="col-lg-12 column_video">
 						<?php echo $video_iframe; ?>
+						<?php if ( $video_cover_img ) : ?>
+							<div class="video_cover">
+								<button onclick="playVideo()">
+									<svg height="100%" version="1.1" viewBox="0 0 68 48" width="100%"><path class="ytp-large-play-button-bg" d="M66.52,7.74c-0.78-2.93-2.49-5.41-5.42-6.19C55.79,.13,34,0,34,0S12.21,.13,6.9,1.55 C3.97,2.33,2.27,4.81,1.48,7.74C0.06,13.05,0,24,0,24s0.06,10.95,1.48,16.26c0.78,2.93,2.49,5.41,5.42,6.19 C12.21,47.87,34,48,34,48s21.79-0.13,27.1-1.55c2.93-0.78,4.64-3.26,5.42-6.19C67.94,34.95,68,24,68,24S67.94,13.05,66.52,7.74z" fill="#f00"></path><path d="M 45,24 27,14 27,34" fill="#fff"></path></svg>
+								</button>
+								<?php display_responsive_image( $video_cover_img, '' ); ?>
+								<script>
+									const video_cover = document.querySelector('.video_cover')
+									let player
+
+									function onYouTubeIframeAPIReady() {
+										player = new YT.Player('yt-player')
+									}
+
+									function playVideo(){
+										player.playVideo()
+										video_cover?.classList.add('hidden')
+									}
+								</script>
+							</div>
+						<?php endif; ?>
 					</div>
 				<?php endif; ?>
 				<?php if ( $video_title ) : ?>
--- functions.php
+++ functions.php
@@ -19,6 +19,7 @@ function theme_enqueue_styles() {
 	// Get the theme data
 	$the_theme = wp_get_theme();
 	wp_enqueue_style( 'child-understrap-styles', get_stylesheet_directory_uri() . '/css/child-theme.min.purged.css', array(), $the_theme->get( 'Version' ) );
+	wp_enqueue_style( 'child-webo-custom-styles', get_stylesheet_directory_uri() . '/css/custom.css', array('child-understrap-styles'), $the_theme->get( 'Version' ) );
 	wp_enqueue_script( 'jquery' );
 	wp_enqueue_script( 'child-understrap-scripts', get_stylesheet_directory_uri() . '/js/child-theme.min.js', array(), $the_theme->get( 'Version' ), true );
 
